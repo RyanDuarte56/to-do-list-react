b7317baf698ef7d939f2ae10e5fe36d585219bf0
RyanDuarte56 <ryandspedrosa@gmail.com>
2025-07-27 12:33:24 +0200

    Upload files

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+function Input(props) {
+  return (
+    <input
+      type="text"
+      className="border border-slate-300 outline-slate-400 px-4 py-2"
+      {...props}
+    />
+  );
+}
+
+export default Input;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,34 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    navigate(`/task?title=${task.title}&description=${task.description}`);
+  }
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <Button
+            onClick={() => onTaskClick(task.id)}
+            className={`text-left w-full ${task.isCompleted && "line-through"}`}
+          >
+            {task.title}
+          </Button>
+          <Button onClick={() => onSeeDetailsClick(task)}>
+            <ChevronRightIcon />
+          </Button>
+          <Button onClick={() => onDeleteTaskClick(task.id)}>
+            <TrashIcon />
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
